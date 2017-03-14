219bda4d3a8af4085543b616c20bf76dec70a991
WITOLD STAFIEJ <soharek@gmail.com>
2017-03-14 16:33:57 +0100

    Add modal imgs

--- script.js
+++ script.js
@@ -68,8 +68,43 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 		});
 
+
+
 	}
 	    
+	//modal img
+
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+//var imgs = $(".player-foto img")
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+$("figure img").click(function(event) {
+	console.log("fotos")
+	modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+	
+});
+
+// img.click = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.click = function() { 
+  modal.style.display = "none";
+}
+
+
 	    
 	    
 });
